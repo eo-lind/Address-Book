f0a66c54c357c5a15eb1b3df5cd94072b2fe845f
Olivia Lind <eo.lind@gmail.com>
2022-05-29 13:18:40 -0500

    commented out previous code in order to start over - also changed namespace for all to AddressBook

--- Contact.cs
+++ Contact.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace AddressBook
 {
-    public class Contact
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
-        public string FullName { get; }
-    }
+    // public class Contact
+    // {
+    //     public string FirstName { get; set; }
+    //     public string LastName { get; set; }
+    //     public string Email { get; set; }
+    //     public string Address { get; set; }
+    //     public string FullName { get; }
+    // }
 
     // public Contact(string firstName, string lastName, string email, string address)
     // {
